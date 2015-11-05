f2ace3b4ed03680dce732901c7bd7b64dbb2f5fa
Hack Reactor Students <accounts@hackreactor.com>
2015-11-05 12:32:23 -0800

    Refactored user.js to mongodb

--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,25 @@
-var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/27017');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log('Connection made!');
 });
+
+var userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  links: [{
+     timeStamp: Date,
+     base_url: String,
+     title: String,
+     code: String,
+     visits: Number 
+  }]
+  
+})
+
+var User = mongoose.model('User', userSchema);
 
 module.exports = User;
