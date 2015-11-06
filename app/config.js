51b8aeaa1cf27f3a6a4421e4f821a1969dd92cd1
Hack Reactor Students <accounts@hackreactor.com>
2015-11-05 19:42:41 -0800

    Change uri settings for mongodb

--- app/config.js
+++ app/config.js
@@ -1,5 +1,28 @@
+// var mongoose = require('mongoose');
+// var mongolabURI = 'mongodb://MongoLab-v:fANoxM62VOmw8MzEw96yKcgDCtVzzmZ3rC5zOYSMR04-@ds052408.mongolab.com:52408/MongoLab-v'
+// var url = process.env.mongolabURI || 'mongodb://localhost/27017';
+// mongoose.connect(url);
+
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/27017');
+var uriUtil = require('mongodb-uri');
+ 
+/* 
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for 
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };       
+ 
+/*
+ * Mongoose uses a different connection string format than MongoDB's standard.
+ * Use the mongodb-uri library to help you convert from the standard format to
+ * Mongoose's format.
+ */
+var mongodbUri = 'mongodb://MongoLab-v:fANoxM62VOmw8MzEw96yKcgDCtVzzmZ3rC5zOYSMR04-@ds052408.mongolab.com:52408/MongoLab-v' || 'mongodb://localhost/27017';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+mongoose.connect(mongooseUri, options);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -8,3 +31,4 @@ db.once('open', function (callback) {
 });
 
 module.exports = mongoose;
+
